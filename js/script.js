d5cb87a28b5091189e199b54f176c42a10e2b23d
alexl <alexlarik13@gmail.com>
2024-01-30 22:33:36 +0200

    Increment the ball’s width & height and updates the text inside accordingly

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 'use strict'
-const elBall = document.querySelector(".ball")
+//
+const gOriginalSize = { currWidth: 100, currHeight: 100 }
+//
 
-function onBallClick(){
-    console.log('hi')
+function onBallClick() {
+    const elBall = document.querySelector(".ball")
+
+    gOriginalSize.currWidth += 50
+    gOriginalSize.currHeight += 50
+
+    elBall.innerText = gOriginalSize.currWidth
+    elBall.style.width = gOriginalSize.currWidth + 'px'
+    elBall.style.height = gOriginalSize.currHeight + 'px'
 }
+// to increment the ball’s width & height by 50px and
