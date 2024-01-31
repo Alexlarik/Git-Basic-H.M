823d16efc42310e9357bd58fb0ddc699ed9393cc
alexl <alexlarik13@gmail.com>
2024-01-31 21:34:52 +0200

    Add a fifth ball chage its layout(color) and edit the loops in script to support the curret code format

--- js/script.js
+++ js/script.js
@@ -21,14 +21,14 @@ function onBallClick(clickedBall) {
 function onThirdBallClick() {
     const elBalls = document.querySelectorAll(".ball")
 
-    for (var i = 0; i < elBalls.length - 2; i++) {
+    for (var i = 0; i < elBalls.length && i < 2; i++) {
         const ball = elBalls[i]
         onBallClick(ball)
     }
 }
 function onFourthBallClick() {
     const elBalls = document.querySelectorAll(".ball")
-    for (var i = 0; i < elBalls.length - 2; i++) {
+    for (var i = 0; i < elBalls.length && i < 2; i++) {
         const ball = elBalls[i]
         reduceBall(ball)
     }
@@ -39,8 +39,8 @@ function reduceBall(ball) {
     var currWidth = parseInt(ball.style.width)
     var currHeight = parseInt(ball.style.height)
 
-    var newWidth = currWidth - getRandomInt(20, 60)
-    var newHeight = currHeight - getRandomInt(20, 60)
+    var newWidth = (currWidth - getRandomInt(20, 60))
+    var newHeight = (currHeight - getRandomInt(20, 60))
 
     newWidth = Math.max(newWidth, minDiameter)
     newHeight = Math.max(newHeight, minDiameter)
