33772109d7ac2f1d491138f6bd6f55ad26dae347
alexl <alexlarik13@gmail.com>
2024-01-31 17:13:13 +0200

    Add a function that changes the ball’s color to a random color each time it is clicked

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ const gOriginalSize = { currWidth: 100, currHeight: 100 }
 function onBallClick() {
     const elBall = document.querySelector(".ball")
 
-    gOriginalSize.currWidth += getRandomInt(20,60)
-    gOriginalSize.currHeight += getRandomInt(20,60)
+    gOriginalSize.currWidth += getRandomInt(20, 60)
+    gOriginalSize.currHeight += getRandomInt(20, 60)
     if (gOriginalSize.currWidth > 400 || gOriginalSize.currHeight > 400) {
         gOriginalSize.currWidth = 100
         gOriginalSize.currHeight = 100
@@ -16,4 +16,5 @@ function onBallClick() {
     elBall.innerText = gOriginalSize.currWidth
     elBall.style.width = gOriginalSize.currWidth + 'px'
     elBall.style.height = gOriginalSize.currHeight + 'px'
+    elBall.style.backgroundColor = getRandomColor()
 }
--- js/util.js
+++ js/util.js
@@ -5,3 +5,13 @@ function getRandomInt(min, max) {
     const maxFloored = Math.floor(max)
     return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled)
 }
+
+function getRandomColor() {
+    let maxVal = 0xFFFFFF
+    let randomNumber = Math.random() * maxVal
+    randomNumber = Math.floor(randomNumber)
+    randomNumber = randomNumber.toString(16)
+    let randColor = randomNumber.padStart(6, 0)
+    return `#${randColor.toUpperCase()}`
+
+}
