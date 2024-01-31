bf3a4a888c1526056f2dc1de0fb26f187a7133d6
alexl <alexlarik13@gmail.com>
2024-01-31 18:01:48 +0200

    Implement maxdiameter to limit ball growth

--- js/script.js
+++ js/script.js
@@ -5,10 +5,11 @@ const gOriginalSize = { currWidth: 100, currHeight: 100 }
 
 function onBallClick() {
     const elBall = document.querySelector(".ball")
+    const maxDiameter = 400
 
     gOriginalSize.currWidth += getRandomInt(20, 60)
     gOriginalSize.currHeight += getRandomInt(20, 60)
-    if (gOriginalSize.currWidth > 400 || gOriginalSize.currHeight > 400) {
+    if (gOriginalSize.currWidth >= maxDiameter || gOriginalSize.currHeight >= maxDiameter) {
         gOriginalSize.currWidth = 100
         gOriginalSize.currHeight = 100
     }
