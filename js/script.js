2b98b61ade50ab7076a3ac9996be922c3bb3f22d
alexl <alexlarik13@gmail.com>
2024-01-31 21:28:10 +0200

    Create a function that reduces the first two balls and limit its reduction to minDiameter

--- js/script.js
+++ js/script.js
@@ -21,10 +21,33 @@ function onBallClick(clickedBall) {
 function onThirdBallClick() {
     const elBalls = document.querySelectorAll(".ball")
 
-    for (var i = 0; i < elBalls.length - 1; i++) {
+    for (var i = 0; i < elBalls.length - 2; i++) {
         const ball = elBalls[i]
         onBallClick(ball)
     }
 }
+function onFourthBallClick() {
+    const elBalls = document.querySelectorAll(".ball")
+    for (var i = 0; i < elBalls.length - 2; i++) {
+        const ball = elBalls[i]
+        reduceBall(ball)
+    }
+}
+function reduceBall(ball) {
+    const minDiameter = 100
 
+    var currWidth = parseInt(ball.style.width)
+    var currHeight = parseInt(ball.style.height)
+
+    var newWidth = currWidth - getRandomInt(20, 60)
+    var newHeight = currHeight - getRandomInt(20, 60)
+
+    newWidth = Math.max(newWidth, minDiameter)
+    newHeight = Math.max(newHeight, minDiameter)
+
+    ball.innerText = newWidth
+    ball.style.width = newWidth + 'px'
+    ball.style.height = newHeight + 'px'
+
+}
 
